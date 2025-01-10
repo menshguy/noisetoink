0b42c44dc0a2da4f5e292ac4ddd67307efbb39d6
Jeff Fenster <fenster.js@gmail.com>
2025-01-09 22:52:10 -0500

    route fix

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="demo" >
+        <Route path="/demo" >
           <Route path="artist" element={<ArtistDemo />} />
           <Route path="customer" element={<CustomerDemo />} />
         </Route>
